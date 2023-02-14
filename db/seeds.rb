84ef05b5ee465b13590e3ea0ccc51ad1d048348c
sutairusu <sutairusu@icloud.com>
2023-02-14 15:48:11 +0900

    Add Faker data and controller.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  Article.create(
+    title: Faker::Lorem.sentence(word_count: 3),
+    content: Faker::Lorem.paragraph(sentence_count: 4)
+  )
+end
